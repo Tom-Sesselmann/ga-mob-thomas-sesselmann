2a12cb015e4603e45d659637a7fee9064dad9346
Thomas Sesselmann <sessel52@gmail.com>
2015-02-28 15:39:18 +1100

    Week 1 assignment start

---
+++ Week1/Profile2/Profile2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Profile2
+//
+//  Created by Thomas Sesselmann on 28/02/2015.
+//  Copyright (c) 2015 Thomas Sesselmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
