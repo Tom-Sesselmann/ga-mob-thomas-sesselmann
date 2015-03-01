c4eb99718d1cd5b8c98804452098b831e2db3230
Thomas Sesselmann <sessel52@gmail.com>
2015-03-01 15:43:43 +1100

    fixed the bug that was causing the modal to go to the wrong page

--- Week1/Profile2/Profile2/ViewController.swift
+++ Week1/Profile2/Profile2/ViewController.swift
@@ -14,10 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    @IBAction func unwindFromSegue(segue: UIStoryboardSegue) {
-        
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
