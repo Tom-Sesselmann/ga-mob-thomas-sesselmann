e35a6ca0d51854e7ca104717c7c9bb1f0645945d
Thomas Sesselmann <thomas.sesselmann@blake.com.au>
2015-03-17 21:03:37 +1100

    some work on goodvsbad

---
+++ wk3/GoodVSBad/GoodVSBad/HealthBar.swift
@@ -0,0 +1,23 @@
+//
+//  HealthBar.swift
+//  GoodVSBad
+//
+//  Created by Thomas Sesselmann on 17/03/2015.
+//  Copyright (c) 2015 ACME Inc. All rights reserved.
+//
+
+import UIKit
+
+class HealthBar: StatusBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+    
+}
---
+++ wk3/GoodVSBad/GoodVSBad/StatusBar.swift
@@ -0,0 +1,23 @@
+//
+//  StatusBar.swift
+//  GoodVSBad
+//
+//  Created by Thomas Sesselmann on 17/03/2015.
+//  Copyright (c) 2015 ACME Inc. All rights reserved.
+//
+
+import UIKit
+
+class StatusBar: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+
+}
