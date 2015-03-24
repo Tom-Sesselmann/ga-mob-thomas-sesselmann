51ac442b2a1c6563234515846cef49f57a6b6b2c
Thomas Sesselmann <thomas.sesselmann@blake.com.au>
2015-03-24 18:19:05 +1100

    added custom cells

--- wk3/GoodVSBad/GoodVSBad/ClassInfoScreen.swift
+++ wk3/GoodVSBad/GoodVSBad/ClassInfoScreen.swift
@@ -11,6 +11,8 @@ import UIKit
 class ClassInfoScreen: UITableViewController {
     
     var className : String?
+    let stats = ["Health", "Mana", "Strength"]
+    let statValues = [100, 80, 10]
     
     @IBOutlet weak var navItem: UINavigationItem!
     
@@ -19,6 +21,44 @@ class ClassInfoScreen: UITableViewController {
         
         self.navItem.title = className
         self.tableView.backgroundView = UIImageView(image: UIImage(named: className!))
+        
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+//        println("\(classList[indexPath.row])")
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        if section == 0 { return stats.count }
+        else            { return 1 }
+        
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("classStatsCell", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel!.text = stats[indexPath.row]
+            cell.detailTextLabel?.text = "\(statValues[indexPath.row])"
+            return cell
+            
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("classDescriptionCell", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel!.text = "Description"
+            cell.detailTextLabel?.text = "This is a really cool class"
+            return cell
+            
+        }
         
     }
 
