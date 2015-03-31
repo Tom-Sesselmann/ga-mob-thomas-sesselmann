7bdf48921dc04c4d70218e17cdca5d61fe7e14d0
Thomas Sesselmann <thomas.sesselmann@blake.com.au>
2015-03-31 19:08:08 +1100

    starting calc app

---
+++ midterm-project/Toms Calculator/Toms Calculator/EqualsButton.swift
@@ -0,0 +1,22 @@
+
+//
+//  EqualsButton.swift
+//  Toms Calculator
+//
+//  Created by Thomas Sesselmann on 31/03/2015.
+//  Copyright (c) 2015 Thomas Sesselmann. All rights reserved.
+//
+
+import UIKit
+
+class EqualsButton: TomsButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ midterm-project/Toms Calculator/Toms Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Toms Calculator
+//
+//  Created by Thomas Sesselmann on 31/03/2015.
+//  Copyright (c) 2015 Thomas Sesselmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var screenLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
