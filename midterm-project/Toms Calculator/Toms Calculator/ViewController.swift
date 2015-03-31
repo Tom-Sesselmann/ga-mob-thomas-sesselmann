676f02870749ae50a9562b5414162c400c4cb48c
Thomas Sesselmann <thomas.sesselmann@blake.com.au>
2015-03-31 20:55:44 +1100

    added code for calculator

--- midterm-project/Toms Calculator/Toms Calculator/ViewController.swift
+++ midterm-project/Toms Calculator/Toms Calculator/ViewController.swift
@@ -12,17 +12,90 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var screenLabel: UILabel!
     
+    var firstNumber : Float = 0.0
+    var secondNumber : Float = 0.0
+    var lastOperation : String = "+"
+    var state : String = "operation"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    @IBAction func digitButtonPressed(sender: DigitButton) {
+        if state == "operation" {
+            state = "number"
+            firstNumber += secondNumber
+            screenLabel.text = ""
+        }
+        screenLabel.text! += "\(sender.titleLabel!.text!)"
+        
+    }
+    
+    
+    @IBAction func operationButtonPressed(sender: OperationButton) {
+        if state == "number" {
+            state = "operation"
+            secondNumber = (screenLabel.text! as NSString).floatValue
+        }
+        lastOperation = sender.titleLabel!.text!
+    }
+    
 
+    @IBAction func equalsButtonPressed(sender: EqualsButton) {
+        if state == "number" {
+            state = "operation"
+            secondNumber = (screenLabel.text! as NSString).floatValue
+        }
+        
+        calculate()
+        
+    }
+    
+    
+    @IBAction func screenSwiped(sender: AnyObject) {
+        screenLabel.text = "0"
+        firstNumber = 0.0
+        secondNumber = 0.0
+        lastOperation = "+"
+        state = "operation"
+    }
+    
+    func calculate() {
+        println("\(firstNumber), \(secondNumber)")
+        switch lastOperation {
+        case "+":
+            firstNumber += secondNumber
+        case "−":
+            firstNumber -= secondNumber
+        case "×":
+            firstNumber *= secondNumber
+        case "÷":
+            if secondNumber == 0 {
+                screenLabel.text = "Error"
+                return
+            }
+            firstNumber /= secondNumber
+        default:
+            println("error")
+        }
+        secondNumber = 0.0
+        
+        // Remove decimal point if number is an int
+        if floor(firstNumber) == firstNumber {
+            screenLabel.text = "\(Int(firstNumber))"
+        } else {
+            screenLabel.text = "\(firstNumber)"
+        }
+        
+    }
 }
 
