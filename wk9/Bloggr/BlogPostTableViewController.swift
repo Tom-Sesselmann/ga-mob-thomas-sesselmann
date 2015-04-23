4ba92929537536cb9bbe097c52ca674631f4e61e
Thomas Sesselmann <thomas.sesselmann@blake.com.au>
2015-04-23 20:19:02 +1000

    more Parse stuff

---
+++ wk9/Bloggr/BlogPostTableViewController.swift
@@ -0,0 +1,126 @@
+//
+//  BlogPostTableViewController.swift
+//  Bloggr
+//
+//  Created by Thomas Sesselmann on 23/04/2015.
+//  Copyright (c) 2015 Thomas Sesselmann. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class BlogPostTableViewController: UITableViewController {
+    
+    var blogPosts = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+        updateTableData()
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        updateTableData()
+    }
+    
+    func updateTableData() {
+        
+        var blogPostQuery = PFQuery(className: "BlogPost")
+        blogPostQuery.whereKey("createdAt", lessThanOrEqualTo: NSDate())
+        blogPostQuery.findObjectsInBackgroundWithBlock() { objects, error in
+            
+            if let results = objects {
+                self.blogPosts = results
+            }
+            self.tableView.reloadData()
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        
+        return blogPosts.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("blogPostCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        if let post = blogPosts[indexPath.row] as? PFObject {
+            cell.textLabel!.text = post.valueForKey("title") as? String
+            cell.detailTextLabel!.text = post.valueForKey("subtitle") as? String
+        }
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- wk9/Bloggr/Bloggr/ViewController.swift
+++ wk9/Bloggr/Bloggr/ViewController.swift
@@ -10,20 +10,76 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var subtitleField: UITextField!
+    @IBOutlet weak var contentField: UITextView!
 
+    @IBAction func savePost(sender: AnyObject) {
+        var blogPost = PFObject(className: "BlogPost")
+        blogPost["title"] = titleField.text
+        blogPost["subtitle"] = subtitleField.text
+        blogPost["content"] = contentField.text
+        blogPost.saveInBackgroundWithBlock() { object, error in
+                self.navigationController!.popViewControllerAnimated(true)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        return
+        
+        // create new object to work with
         var blogPost = PFObject(className: "BlogPost")
+        
+        // set properties to have values
         blogPost["title"] = "Hello World"
         blogPost["subtitle"] = "The classic blog post"
         blogPost["content"] = "Welcome to my blog swifters"
+        
+        // save to Prase some time later
         blogPost.saveInBackground()
         
+        // makes a query for "BlogPost" objects
         var blogPostQuery = PFQuery(className: "BlogPost")
+        
+        // limits the query to only posts created before now
         blogPostQuery.whereKey("createdAt", lessThanOrEqualTo: NSDate())
-//        blogPostQuery.findObjectsi
+        
+        // actually runs the query and gives them back to do something with
+        blogPostQuery.findObjectsInBackgroundWithBlock() { objects, error in
+            
+            // check for existance of returned objects
+            if let results = objects {
+                
+                // loop through all objects from parse
+                for post in results {
+                    
+                    // print out data
+                    println(post["title"])
+                    println(post.createdAt)
+                    
+                    // set title = goodbye
+                    post.setValue("Goodbye", forKey:"title")
+                    
+                    // upload the changes to Parse
+                    post.saveInBackground()
+                }
+            }
+                
+            // Get the firs oject matching the query
+            blogPostQuery.getFirstObjectInBackgroundWithBlock() { object, error in
+                
+                // check an object is there
+                if let blogPost = object {
+                
+                    // delete the object
+                    blogPost.deleteInBackground()
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
