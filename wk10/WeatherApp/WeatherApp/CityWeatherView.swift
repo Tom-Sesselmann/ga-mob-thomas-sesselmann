65ce3e042f13868496fc39e12733033438773c23
Thomas Sesselmann <thomas.sesselmann@blake.com.au>
2015-04-30 19:25:40 +1000

    stuff

--- wk10/WeatherApp/WeatherApp/CityWeatherView.swift
+++ wk10/WeatherApp/WeatherApp/CityWeatherView.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 
+@IBDesignable
 class CityWeatherView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+    @IBInspectable var image: UIImage? {
+        get {
+            return self.weatherImage?.image
+        }
+        set {
+            self.weatherImage?.image = newValue
+        }
     }
-    */
-
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        loadNib()
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadNib()
+        
+    }
+    
+    func loadNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "CityWeatherView", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        
+        view.frame = self.bounds
+        view.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        
+        addSubview(view)
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let path = UIBezierPath(ovalInRect: CGRectMake(20, 20, 150, 150))
+        path.lineWidth = 4
+        UIColor.redColor().setStroke()
+        path.closePath()
+        path.stroke()
+        
+        UIColor.blueColor().setFill()
+        path.fill()
+    }
+    
 }
