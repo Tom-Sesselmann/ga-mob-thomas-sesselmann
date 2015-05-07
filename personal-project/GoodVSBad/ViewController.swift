0243ac271371e6350b2f1c463b42e0c88479bf7c
Thomas Sesselmann <thomas.sesselmann@blake.com.au>
2015-05-07 18:37:22 +1000

    gave a nice animation to damage text and made a game over alert

--- personal-project/GoodVSBad/ViewController.swift
+++ personal-project/GoodVSBad/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var heroHealthBarWidth: NSLayoutConstraint!
     @IBOutlet weak var heroHealthBarCnt: HealthBar!
+    @IBOutlet weak var heroLabel: UILabel!
     @IBOutlet weak var villianHealthBarWidth: NSLayoutConstraint!
     @IBOutlet weak var villianHealthBarCnt: UIView!
+    @IBOutlet weak var villianLabel: UILabel!
     
     
     let superHero = SuperHero(name: "Superman")
@@ -22,6 +24,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        heroLabel.text = superHero.name
+        villianLabel.text = superVillain.name
+        villianLabel.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+        
     }
 
     @IBAction func superHeroAttack(sender: AnyObject) {
@@ -29,21 +35,24 @@ class ViewController: UIViewController {
         println("Super Hero Attacks")
         let damageValue = superHero.attack(superVillain)
         
-        
-        
-        var damageLabel = UILabel(frame: CGRectMake(0, 0, 20, 20))
+        // Make Damage Label
+        var damageLabel = UILabel(frame: CGRectMake(0, 0, 40, 20))
         let x = arc4random_uniform(UInt32(self.view.frame.width) - 50) + 25
-        let y = arc4random_uniform(UInt32(self.view.frame.height/2) - 50) + 50
+        let y = arc4random_uniform(UInt32(self.view.frame.height/2) - 150) + 150
         damageLabel.center = CGPointMake(CGFloat(x), CGFloat(y))
-        
-        
         damageLabel.textAlignment = NSTextAlignment.Center
         damageLabel.text = "\(damageValue)"
+        damageLabel.font = UIFont(name: damageLabel.font.fontName, size: CGFloat(damageValue/2+10))
+        damageLabel.textColor = UIColor.whiteColor()
+        damageLabel.shadowColor = UIColor.blackColor()
+        damageLabel.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
         self.view.addSubview(damageLabel)
         
         
-        UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveLinear, animations: {
+        // Animate Health Bar and Damage Label
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveLinear, animations: {
             damageLabel.alpha = 0
+            damageLabel.center = CGPointMake(CGFloat(x), CGFloat(y - 50))
             self.villianHealthBarWidth.constant = -(self.villianHealthBarCnt.frame.width + 4 - CGFloat(self.superVillain.health / self.superVillain.maxHealth) * self.villianHealthBarCnt.frame.width)
             self.view.layoutIfNeeded()
             
@@ -58,15 +67,22 @@ class ViewController: UIViewController {
         
         let damageValue = superVillain.attack(superHero)
         
-        var damageLabel = UILabel(frame: CGRectMake(0, 0, 20, 20))
-        damageLabel.center = CGPointMake(160, 484)
+        // Make Damage Label
+        var damageLabel = UILabel(frame: CGRectMake(0, 0, 40, 20))
+        let x = arc4random_uniform(UInt32(self.view.frame.width) - 50) + 25
+        let y = arc4random_uniform(UInt32(self.view.frame.height/2) - 150) + UInt32(self.view.frame.height/2)
+        damageLabel.center = CGPointMake(CGFloat(x), CGFloat(y))
         damageLabel.textAlignment = NSTextAlignment.Center
         damageLabel.text = "\(damageValue)"
+        damageLabel.font = UIFont(name: damageLabel.font.fontName, size: CGFloat(damageValue/2+10))
+        damageLabel.textColor = UIColor.whiteColor()
+        damageLabel.shadowColor = UIColor.blackColor()
         self.view.addSubview(damageLabel)
         
-        
-        UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveLinear, animations: {
+        // Animate Health Bar and Damage Label
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveLinear, animations: {
             damageLabel.alpha = 0
+            damageLabel.center = CGPointMake(CGFloat(x), CGFloat(y + 50))
             self.heroHealthBarWidth.constant = self.heroHealthBarCnt.frame.width + 4 - CGFloat(self.superHero.health / self.superHero.maxHealth) * self.heroHealthBarCnt.frame.width
             self.view.layoutIfNeeded()
             
@@ -78,7 +94,12 @@ class ViewController: UIViewController {
     
     func checkGameOver(player : Player) {
         if player.health <= 0 {
-            dismissViewControllerAnimated(true, completion: nil)
+            
+            var alert = UIAlertController(title: "Game Over!", message: "\(player.name) is DEAD", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "New Game", style: UIAlertActionStyle.Default) { alert in
+                self.dismissViewControllerAnimated(true, completion: nil)
+                })
+            self.presentViewController(alert, animated: true, completion: nil)
         }
     }
     
